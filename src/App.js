838d3948930aa9062e841a0bf3d0426c41c09304
gowtham-ggg <gowthamvinoth2015@gmail.com>
2024-11-14 18:31:05 +0530

    Initial commit for MovieRatingApp

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./components/MovieRating/MovieRatingList"
+import MovieRatingList from "./components/MovieRating/MovieRatingList";
+import "./App.css"
+
+
+function App() {
+  return (
+    <div id="container">
+      <MovieRatingList/>
+    </div>
+
+  )
+  
+  
+  
+}
+
+export default App;
